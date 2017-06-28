fdd7a7005ab5c19ad00d6953f5fb6f683c4d01e8
Ishan Rayeen <ishan@Ishans-MacBook-Air.local>
2017-06-28 11:37:08 +0530

    initial parsing done, interviewbit done

--- ojToFile.js
+++ ojToFile.js
@@ -1,45 +1,45 @@
-const fs = require('fs');
-const request = require('request');
-const cheerio = require('cheerio');
-const argv = require('yargs')
-  .option('url',{
-    desc: "url of the problem",
-    alias: 'u',
-    require: true
-  })
-  .option('lang',{
-    desc: "file format (c | cpp | java | py)",
-    alias: 'l',
-    default: 'cpp'
-  })
-  .help()
-  .argv;
+const fs        = require('fs');
+const request   = require('request');
+const parser    = require('./parsers/interviewbit.js');
+const argv      = require('yargs')
+                .option('url',{
+                        desc: "url of the problem",
+                        alias: 'u',
+                        require: true
+                    })
+                .option('lang',{
+                        desc: "file format (c | cpp | java | py)",
+                        alias: 'l',
+                        default: 'cpp'
+                    })
+                .help()
+                .argv;
 
 const extension = argv.lang;
-const url = argv.url;
+const url       = argv.url;
 
 request(url, function(error, response, html){
-  if(!error){
-    var $ = cheerio.load(html);
+    if(!error){
+    if(url.includes('interviewbit')){
+        json = parser.interviewbit(html);
+    }
 
-    var json = { title : "", body: "", code: ""};
+    json.url = url;
+    var data = `/*\nurl: ${json.url}\nTitle: ${json.title}\n${json.body}\n*/\n`;
+    var fileName = json.title+'.'+extension;
+    fileName = fileName.replace(/ /g,'-')
+    fileName = fileName.toLowerCase();
 
-    json.title = $('h1.panel-title.pull-left').text().trim();
-    json.body = $('div#problem-content.markdown-content').text().trim();
-
-    // console.log(json.title);
-    // console.log(json.body);
-
-    var data = '/*\nTitle: ' + json.title + '\n' + json.body + '\n*/\n';
-    var fileName = json.title+extension;
-      fs.writeFileSync(fileName, data, { flag: 'wx' }, (err) =>{
-        if(!err)
-          console.log('Done!');
-        else {
-          console.error(err);
+    fs.writeFile(fileName, data, {flag: 'wx' },(err) => {
+        if(!err){
+            return console.log("The file was saved!");
         }
-      });
+        else if(err.code === 'EEXIST') {
+            console.error(`Error: File with name \"${err.path}\" already exists!`);
+        }
+
+    });
     }else {
-    console.error("Error: Website not found!");
-  }
-})
+        console.error("Error: Website not found!");
+    }
+});
